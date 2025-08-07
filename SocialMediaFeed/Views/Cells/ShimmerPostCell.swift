cfb37bfa6b9b50c9a6fcd41d8a998e1818cd50f1
SoloNineZero <solodyankin.ie@ya.ru>
2025-08-07 17:14:38 +0500

    Визуальные правки ячейки

--- SocialMediaFeed/Views/Cells/ShimmerPostCell.swift
+++ SocialMediaFeed/Views/Cells/ShimmerPostCell.swift
@@ -2,21 +2,21 @@ import UIKit
 
 final class ShimmerPostCell: UITableViewCell {
     static let reuseIdentifier = "ShimmerPostCell"
-
+    
     private let titleView: ShimmerView = {
         let view = ShimmerView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .systemGray5
         return view
     }()
-
+    
     private let bodyView: ShimmerView = {
         let view = ShimmerView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .systemGray5
         return view
     }()
-
+    
     private let avatarView: ShimmerView = {
         let view = ShimmerView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -25,14 +25,14 @@ final class ShimmerPostCell: UITableViewCell {
         view.backgroundColor = .systemGray5
         return view
     }()
-
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupSubviews()
         setupConstraints()
         startAnimation()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -42,7 +42,7 @@ final class ShimmerPostCell: UITableViewCell {
         titleView.startAnimating()
         bodyView.startAnimating()
     }
-
+    
     private func setupSubviews() {
         contentView.addSubview(avatarView)
         contentView.addSubview(titleView)
@@ -53,20 +53,20 @@ final class ShimmerPostCell: UITableViewCell {
         NSLayoutConstraint.activate([
             avatarView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
             avatarView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16),
-            avatarView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -12),
             avatarView.heightAnchor.constraint(equalToConstant: 50),
             avatarView.widthAnchor.constraint(equalToConstant: 50),
-
+            
             titleView.topAnchor.constraint(equalTo: avatarView.topAnchor),
             titleView.leftAnchor.constraint(equalTo: avatarView.rightAnchor, constant: 16),
             titleView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16),
-            titleView.heightAnchor.constraint(equalToConstant: 40),
-
+            titleView.heightAnchor.constraint(equalToConstant: 50),
+            
             bodyView.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 8),
             bodyView.leftAnchor.constraint(equalTo: titleView.leftAnchor),
             bodyView.rightAnchor.constraint(equalTo: titleView.rightAnchor),
             bodyView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12),
-            bodyView.heightAnchor.constraint(equalToConstant: 100),
+            bodyView.heightAnchor.constraint(greaterThanOrEqualToConstant: 120)
+            
         ])
     }
 }
